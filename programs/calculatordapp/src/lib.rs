a33770e7ba86ff1947d50bd0372cee85caeb3b96
web3-diplomat <web3diplomat@yahoo.com>
2024-08-18 06:04:44 -0400

    Specify the context of 'create' function

--- programs/calculatordapp/src/lib.rs
+++ programs/calculatordapp/src/lib.rs
@@ -11,3 +11,12 @@ pub mod calculatordapp {
         Ok(())
     }
 }
+
+#derive(Accounts)
+pub struct create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
